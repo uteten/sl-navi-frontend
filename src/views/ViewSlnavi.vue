4520c3bd3b421551b5949c0ba03bf62ef92e21c0
Uten <git3@uten.jp>
2022-04-29 06:20:20 +0900

    広告2分割

--- src/views/ViewSlnavi.vue
+++ src/views/ViewSlnavi.vue
@@ -52,19 +52,26 @@
       -->
       <event-list ref="appEventList" />
       <links ref="appLinks" />
-      <news ref="appNews" />
       <div
-        v-if="$cookies.get('dev')!=1 && !isLocalhost() && nowT == lastT"
+        v-if="$cookies.get('dev')!=1 && nowT == lastT"
         class="ad"
       >
         <Adsense
           data-ad-client="ca-pub-7267369281211974"
           data-ad-slot="5067544569"
-          ins-style="display:inline-block;width:100%"
+          ins-style="display:inline-block;float:left;width:50%;height:320px;"
+          data-ad-format=""
+          data-full-width-responsive=""
+        />
+        <Adsense
+          data-ad-client="ca-pub-7267369281211974"
+          data-ad-slot="5067544569"
+          ins-style="display:inline-block;width:50%;height:320px;"
           data-ad-format=""
           data-full-width-responsive=""
         />
       </div>
+      <news ref="appNews" />
     </div>
   </div>
 </template>
