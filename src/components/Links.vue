a3c6ebd8ceb01be65e96916bb1b5f86f957c9382
Uten <git3@uten.jp>
2022-05-06 16:40:12 +0900

    公式ブログのtitle自動翻訳

--- src/components/Links.vue
+++ src/components/Links.vue
@@ -6,9 +6,47 @@
     <div class="links_top">
       リンク集
     </div>
-    <h6>初心者向け/総合</h6>
+    <h6>ブログ</h6>
     <dl class="links_menu">
       <dt class="links_item">
+        <a
+          target="_blank"
+          href="https://community.secondlife.com/blogs/"
+        >
+          Secondlife 公式Blog(英語)
+        </a>
+      </dt>
+      <template
+        v-for="z in SLBlogArticles"
+      >
+        <dt
+          :key="z.url"
+          class="links_item right"
+        >
+          <a
+            :key="z.url"
+            target="_blank"
+            :href="z.url"
+          >
+            {{ z.text }}
+          </a>
+        </dt>
+      </template>
+      <dt class="links_item left">
+        <a
+          target="_blank"
+          href="https://danielvoyager.wordpress.com/2022/05/05/old-linden-homes-continents-will-mostly-go-away/"
+        >
+          DANIEL VOYAGER(英語)
+        </a>
+      </dt>
+      <dd class="links_item">
+        :SL最新情報
+      </dd>
+    </dl>
+    <h6>初心者向け/総合</h6>
+    <dl class="links_menu">
+      <dt class="links_item left">
         <a
           target="_blank"
           href="https://aniava.net/"
@@ -19,7 +57,7 @@
       <dd class="links_item">
         :アニメやH、総合
       </dd>
-      <dt class="links_item">
+      <dt class="links_item left">
         <a
           target="_blank"
           href="https://seesaawiki.jp/furrys_habitat/"
@@ -30,7 +68,7 @@
       <dd class="links_item">
         :ケモアバのレビュなど
       </dd>
-      <dt class="links_item">
+      <dt class="links_item left">
         <a
           target="_blank"
           href="https://sljpstartguide.wordpress.com/"
@@ -44,7 +82,7 @@
     </dl>
     <h6>買い物/イベント</h6>
     <dl class="links_menu">
-      <dt class="links_item">
+      <dt class="links_item left">
         <a
           target="_blank"
           href="https://www.seraphimsl.com/"
@@ -56,7 +94,7 @@
         :イベント/セール情報
       </dd>
 
-      <dt class="links_item">
+      <dt class="links_item left">
         <a
           target="_blank"
           href="https://slbloodlines.com/haunts/browse/clubs"
@@ -66,32 +104,6 @@
       </dt>
       <dd class="links_item">
         :今人がいるクラブの一覧
-      </dd>
-    </dl>
-
-    <h6>ブログ</h6>
-    <dl class="links_menu">
-      <dt class="links_item">
-        <a
-          target="_blank"
-          href="https://community.secondlife.com/blogs/"
-        >
-          Secondlife 公式Blog(英語)
-        </a>
-      </dt>
-      <dd class="links_item">
-        :良い景観など
-      </dd>
-      <dt class="links_item">
-        <a
-          target="_blank"
-          href="https://danielvoyager.wordpress.com/2022/05/05/old-linden-homes-continents-will-mostly-go-away/"
-        >
-          DANIEL VOYAGER(英語)
-        </a>
-      </dt>
-      <dd class="links_item">
-        :SL最新情報
       </dd>
     </dl>
   </div>
@@ -106,18 +118,33 @@ export default {
   name: 'Links',
   data: function () {
     return {
-      shops: []
+      SLBlogArticles: []
+    }
+  },
+  mounted () {
+    this.getBloglines()
+  },
+  methods: {
+    async getBloglines () {
+      await axios.get('//sl-navi.com/static/slblog.json').then(res => {
+      // await axios.get('memo.txt').then(res => {
+        this.SLBlogArticles = res.data
+      })
     }
   }
 }
 </script>
 
 <style scorped>
-  dt{
+  dt.left{
     float: left;
   }
+  dt.right{
+    margin-left: 20px;
+    font-weight: normal;
+  }
   dd{
-    margin-left: 80px;
+    margin-left: 20px;
   }
   .links_menu {
     padding: 0.1em 0 0.1em 0;
