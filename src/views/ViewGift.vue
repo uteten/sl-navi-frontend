537cd50c4f545aa28b5fd517c9184c0c418bfc6e
Uten <git3@uten.jp>
2022-05-24 08:59:03 +0900

    giftの広告

--- src/views/ViewGift.vue
+++ src/views/ViewGift.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="col col-12 row">
-    <div class="col-lg-10 order-lg-2 order-1">
+    <div class="col-lg-10 order-lg-1 order-1">
       <gift-list ref="appGiftList" />
     </div>
     <div
-      class="col-lg- order-lg-1 order-2"
+      class="ad col-lg- order-lg-1 order-2"
     >
       <Adsense
         data-ad-client="ca-pub-7267369281211974"
@@ -28,25 +28,9 @@ export default {
 }
 </script>
 <style>
-.row{
-  padding-right: 0px;
-  padding-left: 0px;
-}
-.col-lg-10{
-  padding-left: 0px;
-  padding-right: 0px;
-}
-.col{
-  margin-left: 0px;
-  margin-left: 0px;
-  margin-bottom: 15px;
-}
 .ad{
   text-align: center;
   padding: 0;
   margin-left: 15px;
-  margin-bottom: 15px;
-  width: 100%;
-
 }
 </style>
