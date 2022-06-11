bf661af6c5e99fcf547526b5d2f2531abe0cc858
Uten <git3@uten.jp>
2022-06-11 17:20:21 +0900

    環境修正

--- vue.config-serve.js
+++ vue.config-serve.js
@@ -1,5 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  publicPath: 'https://sl-navi.azureedge.net/static/dist/',
+  publicPath: 'http://localhost:8080/',
   transpileDependencies: true
 })
